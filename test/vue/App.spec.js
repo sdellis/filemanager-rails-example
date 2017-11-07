16de82f379dba0bac5597144e6d0cd052da0e5d1
Shaun Ellis <shaun@sdellis.com>
2017-11-07 17:26:30 -0500

    updates tests

--- test/vue/App.spec.js
+++ test/vue/App.spec.js
@@ -2,7 +2,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import { expect } from 'chai'
 import sinon from 'sinon';
-import { mount } from 'avoriaz'
+import { mount } from 'vue-test-utils'
 import Vuex from 'vuex';
 import App from '../../app/javascript/app.vue'
 import ImageCollection from '../fixtures/vue/images'
@@ -16,9 +16,9 @@ describe('App.vue', () => {
 
   beforeEach(() => {
     actions = {
-      actionClick: sinon.stub(),
-      actionInput: sinon.stub(),
+      handleSelect: sinon.stub(),
     };
+
     store = new Vuex.Store({
       state: {
         startPage: ImageCollection.startPage,
@@ -36,4 +36,22 @@ describe('App.vue', () => {
     const wrapper = mount(App, { store })
     expect(wrapper.is('#filemanager')).to.equal(true)
   })
+
+  it('calls store action handleSelect when a thumbnail is clicked', () => {
+    const wrapper = mount(App, { store })
+    const thumb = wrapper.find('.thumbnail')
+    expect(thumb.is('div')).to.equal(true)
+    thumb.trigger('click')
+    expect(actions.handleSelect.calledOnce).to.equal(true)
+  });
+
+  it('calls store action handleSelect when a thumbnail is shift+clicked', () => {
+    const wrapper = mount(App, { store })
+    const thumb1 = wrapper.findAll('.thumbnail').at(0)
+    const thumb2 = wrapper.findAll('.thumbnail').at(2)
+    thumb1.trigger('click')
+    thumb2.trigger('click.shift')
+    expect(actions.handleSelect.calledOnce).to.equal(false)
+  });
+
 })
