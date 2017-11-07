8ec3fcab207192f3462024fe917747ed5139f7fe
Shaun Ellis <shaun@sdellis.com>
2017-11-07 11:44:33 -0500

    moves components into rails app for simplicity and testing purposes

---
+++ app/javascript/components/Controls.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="controls">
+    <div v-if="orderChanged" id="orderChangedIcon" class="alert alert-info" role="alert">
+      <i class="fa fa-exchange"></i> Page order has changed.
+    </div>
+    <button @click="save" id="save_btn" type="button" class="btn btn-lg btn-primary" :disabled="isDisabled">
+      Save
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'controls',
+  data: function () {
+    return {
+      hidden: this.orderChanged
+    }
+  },
+  computed: {
+    orderChanged: function () {
+      var ogOrder = JSON.stringify(this.$store.state.ogImages.map(img => img.id))
+      var imgOrder = JSON.stringify(this.$store.state.images.map(img => img.id))
+      return ogOrder !== imgOrder
+    },
+    isDisabled: function () {
+      if (this.orderChanged || this.$store.state.changeList.length > 0) {
+        return false
+      } else {
+        return true
+      }
+    }
+  },
+  methods: {
+    save: function () {
+      let body = {
+        startpage: this.$store.state.startPage,
+        thumbnail: this.$store.state.thumbnail,
+        images: this.$store.state.images
+      }
+      this.$store.dispatch('saveState', body)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ app/javascript/components/Detail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="sidebar">
+    <div id="detail" class="actions">
+      <img id="detail_img" :src="selectedImageUrl"></img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detail',
+  computed: {
+    selectedImageUrl () {
+      if (this.$store.state.selected.length === 1) {
+        return this.$store.state.selected[0].url
+      } else {
+        return ''
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+#detail {
+  height: 100%;
+  padding: 15px;
+}
+
+#detail img {
+  height: 100%;
+}
+</style>
